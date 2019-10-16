06cea623dcdc0f73f4069d6ae5ad75384bf9274e
IAmRDhar <rahul.dhar47@gmail.com>
2019-10-17 00:01:51 +0530

    basic example with a GET request

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ import (
 	"github.com/gomodule/redigo/redis"
 )
 
+// HMSET album:1 title "Electric Ladyland" artist "Jimi Hendrix" price 4.95 likes 8
+// HMSET album:2 title "Back in Black" artist "AC/DC" price 5.95 likes 3
+// HMSET album:3 title "Rumours" artist "Fleetwood Mac" price 7.95 likes 12
+// HMSET album:4 title "Nevermind" artist "Nirvana" price 5.95 likes 8
+// ZADD likes 8 1 3 2 12 3 8 4
+
 func main() {
 	// Initialize a connection pool and assign it to the pool global
 	// variable.
